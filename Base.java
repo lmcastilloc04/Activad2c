f96843b97403e240c4dafcb2ef763dccb035d35f
lmcastilloc04 <luis.castillo51229@ucaldas.edu.co>
2023-11-08 20:53:11 -0500

    nuevos trabajos

---
+++ Base.java
@@ -0,0 +1,9 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Base {
+
+        public static PrintStream screen = System.out;
+
+        public static Scanner keyboard = new Scanner(System.in);
+}
